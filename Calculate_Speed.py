5d48b70f59c345d024c3f29a8dc309aa31c1d3db
Tuna <vhtuananh020402@gmail.com>
2023-10-16 13:07:30 +0700

    MBNP-70-Update-env-info

--- Calculate_Speed.py
+++ Calculate_Speed.py
@@ -1,7 +1,23 @@
-from gtfs_functions import Feed, map_gdf
+# Install package
+# pip install gtfs_functions
+
+# Import package
+from gtfs_functions import Feed
+import pandas as pd 
 
 gtfs_path = 'data/morning/hanoi_gtfs_v2_am.zip'
 feed = Feed(gtfs_path, time_windows=[0, 6, 10, 12, 16, 19, 24])
-
+routes = feed.routes
+trips = feed.trips
+stops = feed.stops
+stop_times = feed.stop_times
+shapes = feed.shapes
+stop_freq = feed.stops_freq
+line_freq = feed.lines_freq
 speeds = feed.avg_speeds
-speeds.head(1)
+
+# print(stop_freq.head(4))
+# print(speeds.head(2))
+
+# extract the data
+speeds.to_csv("speed.csv")
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # bus network
 
 this is the readme file of the project
+
+python 3.11.4
+gtfs-functions 2.0.8
