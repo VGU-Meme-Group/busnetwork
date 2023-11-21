93d00a263d7648ad0c6686e2a37684a8022bcaf0
Tuna <vhtuananh020402@gmail.com>
2023-11-21 22:30:47 +0700

    Delete redundancy

--- backend/application/use-cases/bus/findAll.js
+++
@@ -1,7 +0,0 @@
-async function findAll(repository, collectionName) {
-    const collection = repository.collection(collectionName)
-    const query = await collection.find().toArray()
-    return query
-}
-
-exports.findAll =  findAll
--- backend/application/use-cases/segment/findById.js
+++
@@ -1,8 +0,0 @@
-async function findByShapeId(repository, param, collectionName) {
-    const database = repository.db("Cluster0")
-    const collection = database.collection(collectionName)
-    const query = await collection.find({shapeId : param}).toArray();
-    return query
-}
-
-exports.findByShapeId =  findByShapeId
